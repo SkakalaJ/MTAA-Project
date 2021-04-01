9026f3810b370ad432ff8c84eebb77c9071bb18d
Juraj Ďurej <jdurej@192.168.2.109>
2021-04-01 20:24:16 +0200

    room create + update + delete

--- src/server/api/controllers/RoomController.ts
+++ src/server/api/controllers/RoomController.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { TLoginRequest, TLoginResponse } from '../types';
 import { ELoginError } from '../types/errors';
 
-import * as Room from '../../../models/Room';
+import * as Room from '../../../entities/Room';
 import * as UserModel from '../../../models/User';
 import * as RoomModel from '../../../models/Room';
 import { TRoom } from '../types';
@@ -11,18 +11,39 @@ import { TRoom } from '../types';
 export async function createRoom(req: Request, res: Response){
     const body: TRoom = req.body;
 
-    RoomModel.create(body);
+    let room;
+    try{
+        room = await RoomModel.create({name: body.name, avatar: body.avatar});
+    }catch(err){
+        return res.status(401).send(err.message);
+    }
+
+    return room;
 }
 
 export async function updateRoom(req: Request, res: Response){
 
     const body: TRoom = req.body;
 
-    RoomModel.updateNameOfRoom(body.name, body.id);
-
+    let room;
+    try{
+        room = await RoomModel.updateNameOfRoom(body.name, body.id);
+    }catch(err){
+        return res.status(401).send(err.message);
+    }
+    return room;
 }
 
 export async function deleteRoom(req: Request, res: Response){
+    
     const body: TRoom = req.body;
-    RoomModel.destroy(body.id);
+    
+    let isDeleted;
+    try{
+        isDeleted = RoomModel.destroy(body.id);
+    }catch(err){
+        return res.status(401).send(err.message);
+    }
+
+    return isDeleted;
 }
