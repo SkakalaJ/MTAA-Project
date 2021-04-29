7d16c31668a448553c39def07536aa855dbe6bd7
SkakalaJ <juraj.skakala1@gmail.com>
2021-04-29 14:44:47 +0200

    Small fix axios client.

--- Messenger-Frontend/src/api/client.ts
+++ Messenger-Frontend/src/api/client.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { AxiosRequestConfig } from 'axios';
 
 export const instance = axios.create({
     baseURL: 'http://localhost:4000/api/',
@@ -23,15 +22,15 @@ export class Post{
     }
 
     postLogout = async (body: any) => {
-        instance.post('/users/logout', body);
+        return await instance.post('/users/logout', body);
     }
 
     postMessage = async (roomId: number, body: any) => {
-        instance.post('/users/' + roomId + '/messages/', body)
+        return await instance.post('/users/' + roomId + '/messages/', body)
     }
 
     postRoom = async (body: any) => {
-        instance.post('/rooms/', body);
+        return await instance.post('/rooms/', body);
     }
 
 }
@@ -39,11 +38,11 @@ export class Post{
 export class Put{
 
     updatePassword = async (body: any) => {
-        instance.put('/users/password', body);
+        return await instance.put('/users/password', body);
     }
 
     updateRoom = async (roomId: number, body: any) => {
-        instance.put('/rooms/' + roomId, body);
+        return await instance.put('/rooms/' + roomId, body);
     }
 
 }
@@ -51,19 +50,21 @@ export class Put{
 export class Get{
 
     getUserById = async (userId: number) => {
-        instance.get('/users/' + userId);
+        return await instance.get('/users/' + userId);
     }
 
     getMessages = async (roomId: number, query: any) => {
-        instance.get('/rooms/' + roomId + '/messages/', { params: query });
+        return await instance.get('/rooms/' + roomId + '/messages/', { params: query });
     }
 
 }
 
 export class Delete{
+
     deleteMessage = async (roomId: number, messageId: number) => {
-        instance.delete('/rooms/' + roomId + '/messages/' + messageId);
+        return await instance.delete('/rooms/' + roomId + '/messages/' + messageId);
     }
+
 }
 
 export const post = new Post();
