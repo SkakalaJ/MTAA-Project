9812cc208178850212fbe0391133efe85e524e09
Juraj Ďurej <jdurej@Jurajs-MacBook-Pro.local>
2021-04-30 04:27:02 +0200

    password change

--- Messenger-Frontend/src/screens/profile/ChangePasswordScreen.tsx
+++ Messenger-Frontend/src/screens/profile/ChangePasswordScreen.tsx
@@ -16,12 +16,9 @@ import { useComponentWidth } from '../../hooks/useWidth';
 
 import { NavParamList, StackNavProp } from '../../navigation/Navigator';
 import { IAppState } from '../../store';
-import Colors from '../../constants/colors';
-import CustomButton from '../../view/Button';
-import TextIn from '../../view/TextInput';
-import { SpacedContainer } from '../Container';
 import { Formik, FormikProps } from 'formik';
-
+import * as client from '../../api/client';
+import { useAlert } from "react-alert";
 
 const mapStateToProps = (state: IAppState) => {
     return {
@@ -38,8 +35,32 @@ const mapDispatchToProps = (dispatch: Dispatch) => {
 type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps> & StackNavProp<NavParamList, 'Welcome'>;
 
 const ChangePasswordScreenComponent = (props: Props) => {
-    
+
+    const alert = useAlert();
     const [width, onLayout, ready] = useComponentWidth();
+
+    const formik = React.useRef<FormikProps<{ old_password: string; new_password: string }>>(
+        null
+    );
+
+    const changePassword = async (new_password: string, old_password: string) => {
+        const body = {
+            userBid: 'USR00000001',
+            password: old_password,
+            newPassword: new_password
+        }
+            console.log(body);
+        try{
+            var res = await client.put.updatePassword(body, '1ae84552-780c-4868-9afe-3d1e676852bc');
+            
+            props.navigation.navigate('Profile');
+        }catch(err){
+            
+            alert.error(err.response.data.error);    
+        }
+    };
+
+
     return (
         <KeyboardAvoidingView
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -63,23 +84,60 @@ const ChangePasswordScreenComponent = (props: Props) => {
                 <Content>
                     <View style={{ paddingTop:80 }}>
                         <ListItem avatar>
-                            <Body style={styles.middle}>
+                            <Formik
+                            innerRef={formik}
+                            initialValues={{ new_password: '', old_password: '' }}
+                            onSubmit={(values) =>
+                                changePassword(values.new_password, values.old_password)
+                            }
+                            >
+                                {({
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            values,
+                            errors,
+                            touched,
+                            }) => (
                             
+                            <Body style={styles.middle}>
                                 <Item>
                                     <Input placeholder={'old password'}
+                                        textContentType={'password'}
+                                        autoCompleteType={'password'}
+                                        autoCapitalize={'none'}
+                                        autoCorrect={false}
+                                        secureTextEntry={true}
+                                        value={values.old_password}
+                                        onBlur={handleBlur('old_password')}
+                                        onChangeText={handleChange('old_password')}
                                         style={{ marginBottom: 10, marginTop:30 }}/>
                                 </Item>
                                 <Item>
-                                    <Input placeholder={'new password'}
+                                    <Input 
+                                        placeholder={'new password'}
+                                        textContentType={'password'}
+                                        autoCompleteType={'password'}
+                                        autoCapitalize={'none'}
+                                        autoCorrect={false}
+                                        secureTextEntry={true}
+                                        value={values.new_password}
+                                        onBlur={handleBlur('new_password')}
+                                        onChangeText={handleChange('new_password')}
                                         style={{ marginBottom: 10, marginTop:30 }}/>
                                 </Item>
 
                                <Button block 
                                 style={{ marginBottom: 10, marginTop:50 }}
+                                onPress={handleSubmit}
                                 >
                                     <Text> Change </Text>
                                 </Button>
                             </Body>
+
+                            )}
+                            </Formik>
+                           
                           
                         </ListItem>
                     </View>
