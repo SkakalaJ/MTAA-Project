ae58af119cc324123389afba67277dd286a258d2
SkakalaJ <juraj.skakala1@gmail.com>
2021-04-30 02:29:54 +0200

    FE - client update

--- Messenger-Frontend/src/api/client.ts
+++ Messenger-Frontend/src/api/client.ts
@@ -57,6 +57,10 @@ export class Get{
         return await instance.get('/users', { headers: {'Authorization': 'Bearer ' + token}});
     }
 
+    getRoomsAll = async (token: string) => {
+        return await instance.get('/users/rooms', { headers: {'Authorization': 'Bearer ' + token}});
+    }
+
     getMessages = async (roomId: number, query: any, token: string) => {
         return await instance.get('/rooms/' + roomId + '/messages/', { params: query, headers: {'Authorization': 'Bearer ' + token} });
     }
